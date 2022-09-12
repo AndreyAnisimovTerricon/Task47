764fa87f36b6c8b09d3dd7d4e46b0cef60b5dd15
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-12 20:29:37 +0300

    Массив случайных чисел (три строки ,четыре столбца)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int m = 3;
+int n  = 4;
+double[,] GetArray(int m, int n, double minValue, double maxValue) {
+double[,] result = new double[m,n];
+for( int i = 0;i < m;++i ) {
+  for (int j =0;j < n;++j ) {
+    result[i,j] = new Random().NextDouble()*100;
+  }
+}
+return result;
+}
+
+ void PrintArray(double[,] array) {
+    for (int i =0; i < array.GetLength(0);++i) {
+        for (int j = 0; j < array.GetLength(1);++j) {
+            Console.Write(Math.Round(array[i,j],1)+ " ");
+            
+        }
+        Console.WriteLine();
+
+    }
+}
+double[,] result = GetArray(m,n,-100,100);
+PrintArray(result);
